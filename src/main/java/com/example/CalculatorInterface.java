97bd772682c3b52c42e1120b6353959b69493325
ines besrour <87935896+ines-besrour@users.noreply.github.com>
2024-11-08 13:00:40 +0100

    Create CalculatorInterface.java

---
+++ src/main/java/com/example/CalculatorInterface.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public interface CalculatorInterface {
+    int add(int a, int b);
+    int subtract(int a, int b);
+    int multiply(int a, int b);
+    int divide(int a, int b);
+    int square(int a);
+    int power(int a, int b);
+    int absoluteValue(int a);
+    double racineCarree(int a);
+}
