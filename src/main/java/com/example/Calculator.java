bd87bb2a9010158bcdd4ea3cf52bf976452a6e00
ines besrour <87935896+ines-besrour@users.noreply.github.com>
2024-11-08 13:01:34 +0100

    Update Calculator.java

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,6 +1,6 @@
 package com.example;
 
-public class Calculator {
+public class Calculator implements CalculatorInterface{
     public int add(int a, int b) {
         return a + b;
     }
@@ -20,6 +20,28 @@ public class Calculator {
             throw new IllegalArgumentException("Cannot divide by zero");
         }
         return a / b;
+    }
+    @Override
+    public int square(int a) {
+        return a * a;
+    }
+
+    @Override
+    public int power(int a, int b) {
+        return (int) Math.pow(a, b);
+    }
+
+    @Override
+    public int absoluteValue(int a) {
+        return Math.abs(a);
+    }
+
+    @Override
+    public double racineCarree(int a) {
+        if (a < 0) {
+            throw new IllegalArgumentException("Cannot calculate the square root of a negative number");
+        }
+        return Math.sqrt(a);
     }
 
 }
